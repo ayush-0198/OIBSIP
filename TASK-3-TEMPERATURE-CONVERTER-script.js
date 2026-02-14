c2aace82ead00214e55348a2e4a899ee15f4b361
Ayush Prajapati <ayushprajapati0198@gmail.com>
2026-02-14 19:39:04 +0530

    Create TASK-3-TEMPERATURE-CONVERTER-script.js

---
+++ TASK-3-TEMPERATURE-CONVERTER-script.js
@@ -0,0 +1,20 @@
+function convertTemp() {
+
+    var temp = document.getElementById("temperature").value;
+    var unit = document.getElementById("unit").value;
+    var result = document.getElementById("result");
+
+    if (temp === "") {
+        result.innerHTML = "Please enter a temperature value";
+        return;
+    }
+
+    if (unit === "celsius") {
+        var fahrenheit = (temp * 9/5) + 32;
+        result.innerHTML = temp + "°C = " + fahrenheit.toFixed(2) + "°F";
+    } 
+    else {
+        var celsius = (temp - 32) * 5/9;
+        result.innerHTML = temp + "°F = " + celsius.toFixed(2) + "°C";
+    }
+}
